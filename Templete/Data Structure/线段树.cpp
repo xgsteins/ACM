aae92625a3f0d63cc613fb6b7df8f87278a674e4
sudekidesu <33283268+sudekidesu@users.noreply.github.com>
2018-08-21 23:09:48 +0800

    更新

--- Templete/Data Structure/线段树.cpp
+++ Templete/Data Structure/线段树.cpp
@@ -1,8 +1,9 @@
 //      简单线段树
 //      建树 O(nlog(n))
 //      查询 O(log(n))
-const int maxm = 100000 + 100;
+const int maxm = 1000000 + 100;//点的数量
 int num[maxm];
+int b[maxm];
 int sum;
 struct tree
 {
@@ -13,12 +14,22 @@ void push_up(int k)
 {
 		t[k].n = t[k<<1].n + t[k<<1|1].n;
 }
+void refresh(int k)//点标号
+{
+  k=b[k]>>1;
+  while(k>0)
+  {
+    push_up(k);
+    k=k>>1;
+  }
+}//快速点更新
 void make(int l,int r,int k)//1，n，1
 {
 		if(l==r)
 		{
 			t[k].left=t[k].right=l;
 			t[k].n=num[k];
+			b[l]=k;
 			return;
 		}
 		t[k].left=l;
@@ -40,18 +51,10 @@ void Add(int l,int r,int k,int x)//左端点，右端点，1，加x
 				Add(l,r,k<<1|1,x);
 		push_up(k);
 }//段加法
-void add(int d,int k,int x)//点标号，1，加x
+void add(int d,int x)//点标号，加x
 {
-		if(t[k].left==t[k].right)
-		{
-			t[k].n+=x;
-			return;
-		}
-		if(d<=(t[k].left+t[k].right)>>1)
-				add(d,k<<1,x);
-		if(d>(t[k].left+t[k].right)>>1)
-				add(d,k<<1|1,x);
-		push_up(k);
+		num[d]+=x;
+		refresh(d);
 }//点加法
 void Sub(int l,int r,int k,int x)//左端点，右端点，1，减x
 {
@@ -66,18 +69,10 @@ void Sub(int l,int r,int k,int x)//左端点，右端点，1，减x
 				Sub(l,r,k<<1|1,x);
 		push_up(k);
 }//段减法
-void sub(int d,int k,int x)//点标号，1，减x
+void sub(int d,int x)//点标号，减x
 {
-		if(t[k].left==t[k].right)
-		{
-			t[k].n-=x;
-			return;
-		}
-		if(d<=(t[k].left+t[k].right)>>1)
-				sub(d,k<<1,x);
-		if(d>(t[k].left+t[k].right)>>1)
-				sub(d,k<<1|1,x);
-		push_up(k);
+		num[d]-=x;
+		refresh(d);
 }//点减法
 void Query(int l,int r,int k)//左端点，右端点，，1
 {

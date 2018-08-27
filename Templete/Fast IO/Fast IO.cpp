739c77f4204a680b0e58442672f389c651916cf6
xgsteins <xgsteins@outlook.com>
2018-08-27 19:48:43 +0800

    change some operators

--- Templete/Fast IO/Fast IO.cpp
+++ Templete/Fast IO/Fast IO.cpp
@@ -5,9 +5,9 @@ inline bool scan_d(T &ret)
     char c;
     int sgn;
     if( c=getchar() , c==EOF ) return 0; //EOF
-    while(c != '−' && ( c<'0' || c>'9')) c=getchar();
-    sgn = (c=='−') ? -1 : 1;
-    ret = (c=='−') ? 0 : (c-'0');
+    while(c != '-' && ( c<'0' || c>'9')) c=getchar();
+    sgn = (c=='-') ? -1 : 1;
+    ret = (c=='-') ? 0 : (c-'0');
     while( c=getchar(), c>='0'&& c<='9' ) ret = ret*10+(c-'0');
     ret *= sgn;
     return 1;

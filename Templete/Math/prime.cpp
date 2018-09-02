c1e40a9abd3be59741d21e96941c0ced9391db35
sudekidesu <33283268+sudekidesu@users.noreply.github.com>
2018-09-02 12:54:32 +0800

    线性筛优化

--- Templete/Math/prime.cpp
+++ Templete/Math/prime.cpp
@@ -1,13 +1,14 @@
 //我也不知道时间复杂度
 #include<cstring>
-bool ifprime[10000000];//1为素数，0为和数
+#define maxm (int)1e7
+bool ifprime[maxm];//true为素数，false为和数
 void prime()
 {
-	int i,k;
 	memset(ifprime,1,sizeof(ifprime));
 	ifprime[0]=ifprime[1]=false;
-	for(i=2;i<10000000;i++)
+	for(int i=2;i<maxm;i++)
 		if(ifprime[i])
-			for(k=2;k*i<10000000;k++)
-				ifprime[k*i]=false;
+			for(int k=2;k*i<maxm;k++)
+				if(prime[k*i])
+					ifprime[k*i]=false;
  }

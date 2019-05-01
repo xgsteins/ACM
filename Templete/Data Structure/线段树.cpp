b054a8421685b0743bcc666010ccecdfe860cc1c
sudekidesu <33283268+sudekidesu@users.noreply.github.com>
2019-05-01 16:54:50 +0800

    not GG!

--- Templete/Data Structure/线段树.cpp
+++ Templete/Data Structure/线段树.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 const int maxm = 1000000 + 100;//点的数量
 int num[maxm];
-int b[maxm];
 int Max,Min,sum;
 struct tree
 {
     int left,right;
     int maxx,minn;
+	int La;
     int sum;
 }t[maxm*3 + 100];
 void push_up(int k)
@@ -21,68 +21,63 @@ void push_up(int k)
     t[k].minn =min( t[k<<1].minn, t[k<<1|1].minn);
     t[k].sum =t[k<<1].sum+t[k<<1|1].sum;
 }
-void refresh(int k)//点标号
-{
-  k=b[k]>>1;
-  while(k>0)
-  {
-    push_up(k);
-    k=k>>1;
-  }
-}//快速点更新
 void make(int l,int r,int k)//1，n，1
 {
-		if(l==r)
-		{
-			t[k].left=t[k].right=l;
-	        t[k].maxx=t[k].minn=t[k].sum=num[l];
-			b[l]=k;//不需要点修改的时候这行可以删掉
-			return;
-		}
-		t[k].left=l;
-		t[k].right=r;
-		make(l,(l+r)>>1,k<<1);
-		make(((l+r)>>1)+1,r,k<<1|1);
-		push_up(k);
+	if(l==r)
+	{
+		t[k].left=t[k].right=l;
+        t[k].maxx=t[k].minn=t[k].sum=num[l];
+		return;
+	}
+	t[k].left=l;
+	t[k].right=r;
+	make(l,(l+r)>>1,k<<1);
+	make(((l+r)>>1)+1,r,k<<1|1);
+	push_up(k);
 }//建树
 void Add(int l,int r,int k,int x)//左端点，右端点，1，加x
 {
-		if(t[k].left==t[k].right)
+	if(t[k].La!=0)
+	{
+		t[k].sum+=t[k].La*(t[k].right-t[k].left+1);
+		if(t[k].right>t[k].left)
 		{
-			t[k].sum+=x;
-			return;
+			t[k<<1].La+=t[k].La;
+			t[k<<1|1].La+=t[k].La;
 		}
-		if(l<=(t[k].left+t[k].right)>>1)
-				Add(l,r,k<<1,x);
-		if(r>(t[k].left+t[k].right)>>1)
-				Add(l,r,k<<1|1,x);
-		push_up(k);
-}//段加法
-void add(int d,int x)//点标号，加x
-{
-		num[d]+=x;
-		refresh(d);
-}//点加法
-void Sub(int l,int r,int k,int x)//左端点，右端点，1，减x
-{
-		if(t[k].left==t[k].right)
+		t[k].La=0;
+	}
+	t[k].sum+=x*(min(t[k].right,r)-max(t[k].left,l)+1);
+	if(t[k].left>=l&&t[k].right<=r)
+	{
+		t[k].maxx+=x;
+		t[k].minn+=x;
+		if(t[k].right>t[k].left)
 		{
-			t[k].sum-=x;
-			return;
+			t[k<<1].La+=t[k].La;
+			t[k<<1|1].La+=t[k].La;
 		}
-		if(l<=(t[k].left+t[k].right)>>1)
-				Sub(l,r,k<<1,x);
-		if(r>(t[k].left+t[k].right)>>1)
-				Sub(l,r,k<<1|1,x);
-		push_up(k);
-}//段减法
-void sub(int d,int x)//点标号，减x
-{
-		num[d]-=x;
-		refresh(d);
-}//点减法
+		return;
+	}
+	if(l<=(t[k].left+t[k].right)>>1)
+		Add(l,r,k<<1,x);
+	if(r>(t[k].left+t[k].right)>>1)
+		Add(l,r,k<<1|1,x);
+	t[k].minn=min(t[k<<1].minn,t[k<<1|1].minn);
+	t[k].maxx=max(t[k<<1].maxx,t[k<<1|1].maxx);
+}//加法
 void Query(int l,int r,int k)//左端点，右端点，，1
 {
+	if(t[k].La!=0)
+	{
+		t[k].sum+=t[k].La*(t[k].right-t[k].left+1);
+		if(t[k].right>t[k].left)
+		{
+			t[k<<1].La+=t[k].La;
+			t[k<<1|1].La+=t[k].La;
+		}
+		t[k].La=0;
+	}
     if(l<=t[k].left&&r>=t[k].right)
     {
         Max=max(Max,t[k].maxx);
@@ -94,4 +89,4 @@ void Query(int l,int r,int k)//左端点，右端点，，1
         Query(l,r,k<<1);
     if(r>(t[k].left+t[k].right)>>1)
         Query(l,r,k<<1|1);
-}//查询段和
+}//查询,查询前要初始化sum，minn，maxx
